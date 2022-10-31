c3e77f78f76ea2d28fd35d2d8fe744fc433fc590
Jhoel Moreno <jhoel.moreno24@gmail.com>
2022-10-31 16:14:12 -0600

    first commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,26 @@
+import './login.scss'
+
+const Login = () => {
+  return (
+    <div className='login' >
+      <div className="card">
+        <div className="left">
+          <h1>Hello world.</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae velit explicabo molestiae corrupti saepe incidunt delectus, dolorem consequatur? Ipsam facere illum aspernatur iusto libero deserunt eum impedit at veritatis nam.</p>
+          <span>Don't you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form >
+            <input type="text" placeholder='Username' />
+            <input type="password" placeholder='Password' />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
